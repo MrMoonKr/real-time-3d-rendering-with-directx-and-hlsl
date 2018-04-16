87d6a89f0a6baf3dc991ddcd0e8179a7d8283f8e
Paul Varcholik <paul.varcholik@ucf.edu>
2018-04-16 15:14:19 -0400

    1.5 - Colored Cube

---
+++ source/1.5_Colored_Cube/RenderingGame.cpp
@@ -0,0 +1,100 @@
+#include "pch.h"
+#include "RenderingGame.h"
+#include "GameException.h"
+#include "KeyboardComponent.h"
+#include "MouseComponent.h"
+#include "GamePadComponent.h"
+#include "FpsComponent.h"
+#include "ColoredCubeDemo.h"
+#include "Grid.h"
+#include "FirstPersonCamera.h"
+
+using namespace std;
+using namespace DirectX;
+using namespace Library;
+
+namespace Rendering
+{
+	RenderingGame::RenderingGame(std::function<void*()> getWindowCallback, std::function<void(SIZE&)> getRenderTargetSizeCallback) :
+		Game(getWindowCallback, getRenderTargetSizeCallback), mRenderStateHelper(*this)
+	{
+	}
+
+	void RenderingGame::Initialize()
+	{
+		mKeyboard = make_shared<KeyboardComponent>(*this);
+		mComponents.push_back(mKeyboard);
+		mServices.AddService(KeyboardComponent::TypeIdClass(), mKeyboard.get());
+
+		mGamePad = make_shared<GamePadComponent>(*this);
+		mComponents.push_back(mGamePad);
+		mServices.AddService(GamePadComponent::TypeIdClass(), mGamePad.get());
+
+		auto camera = make_shared<FirstPersonCamera>(*this);
+		mComponents.push_back(camera);
+		mServices.AddService(Camera::TypeIdClass(), camera.get());
+
+		auto mouse = make_shared<MouseComponent>(*this);
+		mComponents.push_back(mouse);
+		mServices.AddService(MouseComponent::TypeIdClass(), mouse.get());
+
+		auto grid = make_shared<Grid>(*this, camera);
+		mComponents.push_back(grid);
+
+		mColoredCubeDemo = make_shared<ColoredCubeDemo>(*this, camera);
+		mComponents.push_back(mColoredCubeDemo);
+
+		Game::Initialize();
+
+		mFpsComponent = make_shared<FpsComponent>(*this);
+		mFpsComponent->Initialize();
+
+		camera->SetPosition(0.0f, 2.5f, 20.0f);
+	}
+
+	void RenderingGame::Update(const GameTime &gameTime)
+	{
+		mFpsComponent->Update(gameTime);
+
+		if (mKeyboard->WasKeyPressedThisFrame(Keys::Escape) || mGamePad->WasButtonPressedThisFrame(GamePadButtons::Back))
+		{
+			Exit();
+		}
+
+		if (mKeyboard->WasKeyPressedThisFrame(Keys::Space))
+		{
+			mColoredCubeDemo->SetAnimationEnabled(!mColoredCubeDemo->AnimationEnabled());
+		}
+		
+		Game::Update(gameTime);
+	}
+
+    void RenderingGame::Draw(const GameTime &gameTime)
+    {
+		mDirect3DDeviceContext->ClearRenderTargetView(mRenderTargetView.Get(), reinterpret_cast<const float*>(&BackgroundColor));
+		mDirect3DDeviceContext->ClearDepthStencilView(mDepthStencilView.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+		Game::Draw(gameTime);
+
+		mRenderStateHelper.SaveAll();
+		mFpsComponent->Draw(gameTime);
+		mRenderStateHelper.RestoreAll();
+
+		HRESULT hr = mSwapChain->Present(1, 0);
+
+		// If the device was removed either by a disconnection or a driver upgrade, we must recreate all device resources.
+		if (hr == DXGI_ERROR_DEVICE_REMOVED || hr == DXGI_ERROR_DEVICE_RESET)
+		{
+			HandleDeviceLost();
+		}
+		else
+		{
+			ThrowIfFailed(hr, "IDXGISwapChain::Present() failed.");
+		}
+    }
+
+	void RenderingGame::Exit()
+	{
+		PostQuitMessage(0);
+	}
+}
