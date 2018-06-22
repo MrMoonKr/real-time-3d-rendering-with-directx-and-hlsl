7a1cd07eb869f0ba78a7fa8b7fee8c868ae9b8c8
Paul Varcholik <paul.varcholik@ucf.edu>
2018-06-22 11:40:27 -0400

    Windows title fix

--- source/6.1_Normal_Mapping/Program.cpp
+++ source/6.1_Normal_Mapping/Program.cpp
@@ -17,7 +17,7 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int showCommand)
 	SetCurrentDirectory(UtilityWin32::ExecutableDirectory().c_str());
 
 	const wstring windowClassName = L"RenderingClass";
-	const wstring windowTitle = L"3.2 - Diffuse Lighting";
+	const wstring windowTitle = L"6.1 - Normal Mapping";
 
 	const SIZE RenderTargetSize = { 1024, 768 };
 	HWND windowHandle;
